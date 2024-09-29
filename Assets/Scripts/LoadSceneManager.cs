559d63bd04d187a3242c5aa46216ae4cc2799f6a
Aditya <adityacode69@gmail.com>
2024-09-29 21:31:22 +0530

    100 level done

--- Assets/Scripts/LoadSceneManager.cs
+++ Assets/Scripts/LoadSceneManager.cs
@@ -29,7 +29,7 @@ public class LoadSceneManager : MonoBehaviour
         //  Config.Energy=0;
         // Config.SetCoin(1000);
         // Config.ResetLevel();
-        Config.SetCurrLevel(80);
+        Config.SetCurrLevel(100);
         Config.GetSound();
         Config.GetMusic();
         Config.currCoin = Config.GetCoin();
