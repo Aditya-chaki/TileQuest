3b98397a04bb56f7cf0cacfd302a3512a1bee078
Aditya <>
2024-11-08 22:05:56 +0530

    Improved features

--- Assets/Scripts/Map/EventData.cs
+++ Assets/Scripts/Map/EventData.cs
@@ -5,4 +5,8 @@ public class EventData : ScriptableObject
 {
     public Sprite backgroundImage; // The image to display
     public string eventText;       // The text to display
+    public int food;               // Amount of food to deduct
+    public int strength;           // Amount of strength to deduct
+    public int health;             // Amount of health to deduct
+    public int gold;               // Amount of gold to deduct
 }
