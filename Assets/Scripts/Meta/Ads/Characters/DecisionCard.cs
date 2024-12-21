11acab1121fbff80d99d4d2bda5ab833c2e2ecde
Aditya <adityachaki18@gmail.com>
2024-12-21 11:27:19 +0530

    Modified  the decision card

--- Assets/Scripts/Meta/Ads/Characters/DecisionCard.cs
+++ Assets/Scripts/Meta/Ads/Characters/DecisionCard.cs
@@ -14,18 +14,17 @@ public class DecisionCard : MonoBehaviour
 
     [SerializeField] private GameObject card;
 
-    [SerializeField] private DialogueSystem dialogueSystem;
+    
 
-    [SerializeField] private TextMeshProUGUI opinionChangeText;
-    [SerializeField] private int opinionVariable;
+    
     [SerializeField] private int foodVariable;
     [SerializeField] private int strengthVariable;
     [SerializeField] private int healthVariable;
     [SerializeField] private int goldVariable;
-    [SerializeField] private string TutNumber;
+    
 
 
-    public Character characterID;
+   
 
     void Start()
     {
@@ -36,8 +35,7 @@ public class DecisionCard : MonoBehaviour
 
     private void UpdateOpinion(int changeDirection)
     {
-        int opinionValue = characterID.GetOpinion();
-        characterID.SetOpinion(opinionValue + (opinionVariable * changeDirection));
+        
 
         // Update other variables
         Config.Food += foodVariable * changeDirection;
@@ -49,18 +47,11 @@ public class DecisionCard : MonoBehaviour
 
         // Update the text display
         string changeType = changeDirection > 0 ? "Increased" : "Dropped";
-        opinionChangeText.text = $"Opinion {changeType} by {opinionVariable}";
+       
 
-        StartCoroutine(HideOpinionTextAfterDelay(3f));
-         if (dialogueSystem != null)
-        {
-            dialogueSystem.SetInitialInvariant(TutNumber); // Call the setter to change the invariant
-        }
+        
+         
     }
 
-    private IEnumerator HideOpinionTextAfterDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        opinionChangeText.text = ""; // Clear the text after delay
-    }
+    
 }
