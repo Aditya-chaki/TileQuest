12f370a9430987556912049ba519755096e0f6f3
Ash28s <125830669+Ash28s@users.noreply.github.com>
2025-04-12 03:26:39 +0530

    Made chanvges in the decision card and also changed the script of the decision cards

--- Assets/Scripts/Meta/Ads/Characters/DecisionCard.cs
+++ Assets/Scripts/Meta/Ads/Characters/DecisionCard.cs
@@ -1,47 +1,113 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+// using UnityEngine.UI;
+// using TMPro;
+// using VNGame;
+
+// public class DecisionCard : MonoBehaviour
+// {
+//     [SerializeField] private Button optionALoss;
+//     [SerializeField] private Button optionBGain;
+
+//     [SerializeField] private GameObject card;
+
+
+//     [SerializeField] private int foodVariable;
+//     [SerializeField] private int strengthVariable;
+//     [SerializeField] private int healthVariable;
+//     [SerializeField] private int goldVariable;
+
+//     [SerializeField] private int energyVariable;
+
+//     void Start()
+//     {
+//         optionALoss?.onClick.AddListener(() => UpdateOpinion(-1));
+//         optionBGain?.onClick.AddListener(() => UpdateOpinion(1));
+//     }
+
+//     private void UpdateOpinion(int changeDirection)
+//     {
+//         // Update other variables
+//         Config.Food += foodVariable * changeDirection;
+//         Config.Strength += strengthVariable * changeDirection;
+//         Config.Health += healthVariable * changeDirection;
+//         Config.Gold += goldVariable * changeDirection;
+//         Config.Energy += energyVariable * changeDirection;
+
+     
+
+//         // Destroy the decision card after 5 seconds
+//         Destroy(gameObject, 2f);
+
+//         // Update the text display
+//         string changeType = changeDirection > 0 ? "Increased" : "Dropped";
+//     }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using TMPro;
 using VNGame;
 
 public class DecisionCard : MonoBehaviour
 {
-    [SerializeField] private Button optionALoss;
-    [SerializeField] private Button optionBGain;
+    [SerializeField] private Button optionAButton;
+    [SerializeField] private Button optionBButton;
 
-    [SerializeField] private GameObject card;
+    // Option A Effects
+    [Header("Option A Effects")]
+    [SerializeField] private int foodA;
+    [SerializeField] private int goldA;
+    [SerializeField] private int strengthA;
+    [SerializeField] private int healthA;
+    [SerializeField] private int energyA;
+    [SerializeField] private int opinionA;
 
+    // Option B Effects
+    [Header("Option B Effects")]
+    [SerializeField] private int foodB;
+    [SerializeField] private int goldB;
+    [SerializeField] private int strengthB;
+    [SerializeField] private int healthB;
+    [SerializeField] private int energyB;
+    [SerializeField] private int opinionB;
 
-    [SerializeField] private int foodVariable;
-    [SerializeField] private int strengthVariable;
-    [SerializeField] private int healthVariable;
-    [SerializeField] private int goldVariable;
+    [Header("Character ID")]
+    [SerializeField] private string characterId = "queen"; // default, can change per card
 
     void Start()
     {
-        optionALoss?.onClick.AddListener(() => UpdateOpinion(-1));
-        optionBGain?.onClick.AddListener(() => UpdateOpinion(1));
+        optionAButton?.onClick.AddListener(ApplyOptionA);
+        optionBButton?.onClick.AddListener(ApplyOptionB);
     }
 
-    private void UpdateOpinion(int changeDirection)
+    private void ApplyOptionA()
     {
-        // Update other variables
-        Config.Food += foodVariable * changeDirection;
-        Config.Strength += strengthVariable * changeDirection;
-        Config.Health += healthVariable * changeDirection;
-        Config.Gold += goldVariable * changeDirection;
+        ApplyChanges(foodA, goldA, strengthA, healthA, energyA, opinionA);
+    }
 
-        
-        
+    private void ApplyOptionB()
+    {
+        ApplyChanges(foodB, goldB, strengthB, healthB, energyB, opinionB);
+    }
 
-     
+    private void ApplyChanges(int food, int gold, int strength, int health, int energy, int opinionChange)
+    {
+        Config.Food += food;
+        Config.Gold += gold;
+        Config.Strength += strength;
+        Config.Health += health;
+        Config.Energy += energy;
 
-        // Destroy the decision card after 5 seconds
+        int currentOpinion = Config.GetOpinionMeter(characterId);
+        Config.SetOpinionMeter(characterId, currentOpinion + opinionChange);
+
         Destroy(gameObject, 2f);
-
-        // Update the text display
-        string changeType = changeDirection > 0 ? "Increased" : "Dropped";
     }
 }
+
+
