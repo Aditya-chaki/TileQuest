3d6a5790eabcee47a69509008d0bdebc85578476
Prem Gavhane <gavhaneprem23@gmail.com>
2025-04-20 23:21:28 +0530

    daily quest update

--- Assets/Scripts/Meta/Story/SceneEnabler.cs
+++ Assets/Scripts/Meta/Story/SceneEnabler.cs
@@ -5,28 +5,53 @@ using UnityEngine.SceneManagement;
 
 public class SceneEnabler : MonoBehaviour
 {
-    public GameObject scene;
+  
+    public GameObject Quest_List_scene;
+    public GameObject Daily_Quest_Scene;
+    public GameObject Weekly_Quest_scene;
+
+    //**********************************************
+    public void Quest_list_Enable()
+    {
+        Scene_Enable_Disable(true, false, false);
+    }
    
-    public void SceneDisable()
+   
+    //************************************************
+    public void Daily_Quest_Enable()
     {
-        scene.SetActive(false);
+        Scene_Enable_Disable(false, true, false);
     }
-    public void SceneEnable()
+  
+ 
+    //*************************************************
+    public void Weekly_Quest_Enable()
     {
-        scene.SetActive(true);
+        Scene_Enable_Disable(false, false, true);
     }
-    
+
+ 
+   
+    //*************************************************
     public void MenuEnabler()
     {
          SceneManager.LoadScene("Menu");
     }
+
+    //************************************************
     public void Pay(){
         
         if (Config.currCoin >= 10){
-            scene.SetActive(false);
+            Daily_Quest_Scene.SetActive(false);
         }
         
         Config.SetCoin(Config.currCoin - 10 );
     }
 
+    public void Scene_Enable_Disable(bool quest_list,bool daily_quest,bool weekly_quest )
+    {
+        Quest_List_scene.SetActive(quest_list);
+        Daily_Quest_Scene.SetActive(daily_quest);
+        Weekly_Quest_scene.SetActive(weekly_quest);
+    }
 }
