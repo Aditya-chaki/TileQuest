7a96f11f8518d1d72147df213318caca83742799
vishal <vishal80853@gmail.com>
2025-05-31 16:36:38 +0530

    Changed minigames gameover panel and added progessbar to the weekly quest

--- Assets/Scripts/Quests/WeeklyQuest.cs
+++ Assets/Scripts/Quests/WeeklyQuest.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using TMPro;
 using System.Linq;
+using UnityEngine.UI;
 public class WeeklyQuest : MonoBehaviour
 {
     #region Constants and Fields
@@ -28,19 +29,25 @@ public class WeeklyQuest : MonoBehaviour
     private const string WEEKLY_RESET_TIME_PREF_KEY = "WeeklyQuestResetTime";
     private const string ACTIVE_QUESTS_KEY = "WeeklyActiveQuests";
 
-    
+    private const string WEEKLY_QUETS_COMPLETED = "WeeklyQuestComplete";
+    private const string QUEST_REWARD_CLAIMED_KEY = "WeeklyQuestRewardClaimed_"; // Suffix with milestone (1, 2, 3)
+    private static int numOfQuest = 3;
+    private static int completedQuest;
+    private static readonly int[] REWARD_MILESTONES = { 1, 2, 3 }; // Quests completed for rewards
+    private static readonly int[] MILESTONE_REWARDS = { 200, 400, 600 }; // Coins for each milestone
+    public Slider questProgressSlider;
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        completedQuest = PlayerPrefs.GetInt(WEEKLY_QUETS_COMPLETED,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     questProgressSlider.value = completedQuest/numOfQuest;   
     }
 
     public static string GetQuestName(int questNumber)
@@ -113,6 +120,7 @@ public class WeeklyQuest : MonoBehaviour
         int currentCoins = Config.GetCoin();
         Config.SetCoin(currentCoins + REWARD_COINS_QUEST1);
         Debug.Log($"Quest 1 completed! Player rewarded with {REWARD_COINS_QUEST1} coins.");
+        UpdateCompletedQuests();
         PlayerPrefs.Save();
     }
 
@@ -162,6 +170,7 @@ public class WeeklyQuest : MonoBehaviour
         int currentCoins = Config.GetCoin();
         Config.SetCoin(currentCoins + REWARD_COINS_QUEST2);
         Debug.Log($"Quest 2 completed! Player rewarded with {REWARD_COINS_QUEST2} coins.");
+        UpdateCompletedQuests();
         PlayerPrefs.Save();
     }
 
@@ -205,6 +214,7 @@ public class WeeklyQuest : MonoBehaviour
         int currentCoins = Config.GetCoin();
         Config.SetCoin(currentCoins + REWARD_COINS_QUEST3);
         Debug.Log($"Quest 3 completed! Player rewarded with {REWARD_COINS_QUEST3} coins.");
+        UpdateCompletedQuests();
         PlayerPrefs.Save();
     }
 
@@ -232,8 +242,47 @@ public class WeeklyQuest : MonoBehaviour
         ResetQuest3();
         Debug.Log("All weekly quests have been reset and new quests selected.");
     }
+    #endregion
 
     
+
+    #region Quest Milestone Completion Rewards
+
+    private static void UpdateCompletedQuests()
+    {
+        int completedCount = PlayerPrefs.GetInt(WEEKLY_QUETS_COMPLETED, 0);
+        completedCount++;
+        completedQuest = completedCount;
+        PlayerPrefs.SetInt(WEEKLY_QUETS_COMPLETED, completedCount);
+        PlayerPrefs.Save();
+        CheckMilestoneRewards(completedCount);
+    }
+
+    private static void CheckMilestoneRewards(int completedCount)
+    {
+        for (int i = 0; i < REWARD_MILESTONES.Length; i++)
+        {
+            if (completedCount == REWARD_MILESTONES[i] && !PlayerPrefs.HasKey(QUEST_REWARD_CLAIMED_KEY + REWARD_MILESTONES[i]))
+            {
+                GrantMilestoneReward(REWARD_MILESTONES[i], MILESTONE_REWARDS[i]);
+            }
+        }
+    }
+
+    private static void GrantMilestoneReward(int milestone, int coins)
+    {
+        PlayerPrefs.SetInt(QUEST_REWARD_CLAIMED_KEY + milestone, 1);
+        int currentCoins = Config.GetCoin();
+        Config.SetCoin(currentCoins + coins);
+        Debug.Log($"Milestone reward granted for completing {milestone} weekly quest(s): {coins} coins.");
+        PlayerPrefs.Save();
+    }
+
+    public static (int current, int total) GetQuestCompletionProgress()
+    {
+        int completedCount = PlayerPrefs.GetInt(WEEKLY_QUETS_COMPLETED, 0);
+        return (Mathf.Min(completedCount, numOfQuest), numOfQuest);
+    }
 
     #endregion
 }
